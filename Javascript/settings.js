2447d756fbe28af687106b75d515e6fac4f2596e
Mohak Jain <mohakj500@gmail.com>
2023-05-29 23:50:25 +0530

    Demo Working

--- Javascript/settings.js
+++ Javascript/settings.js
@@ -3,6 +3,7 @@ var settingsButton = document.getElementById("settingsButton");
 var themeSelector = document.getElementById("theme");
 var background = document.getElementById("background");
 
+//Change theme
 themeSelector.onchange = function () {
     background.src = "Art/Images/" + themeSelector.value + ".jpg";
 }
--- Javascript/sounds.js
+++ Javascript/sounds.js
@@ -1,10 +1,26 @@
-var audioFiles = [musicPath('MusicThree'), musicPath('MusicOne'), musicPath('MusicTwo')];
+var audioFiles = [musicPath('MusicOne'), musicPath('MusicTwo'), musicPath('MusicThree'), musicPath('MusicFour')];
+autoplay(0, audioFiles);
 
+function autoplay(i, list) {
+    var sound = new Howl({
+        src: [list[i]],
+        preload: true,
+        onend: function () {
+            if ((i + 1) == list.length) {
+                autoplay(0, list)
+            } else {
+                autoplay(i + 1, list)
+            }
+        }
+    })
+    sound.play();
+}
 
 function musicPath(filename) {
     return 'Art/Sounds/' + filename + '.mp3';
 }
 
+//Adjust Volume
 const volumeSlider = document.getElementById("volumeSlider");
 volumeSlider.onchange = changeVolume;
 function changeVolume() {
